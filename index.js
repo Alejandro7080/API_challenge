2dafe8741ce86400dbbc0dbb35d65de11e3e79df
TXT-39 <txt-39@MacBook-Pro-42.attlocal.net>
2021-06-30 09:30:19 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = 4009;
+
+app.get("/activity", async function (request, response){
+ const api_url = "http://www.boredapi.com/api/activity/";
+ const fetch_response = await fetch(api_url);
+ const jsonObj = await fetch_response.json();
+ response.json(jsonObj);
+});
+app.get("/param/:activity", async function (request, response){
+    const activity = request.params.activity;
+    const api_url = `http://www.boredapi.com/api/activity?type=${activity}`;
+    const fetch_response = await fetch(api_url);
+    const jsonObj = await fetch_response.json();
+    response.json(jsonObj);
+})
+app.get("/only_activity/:activity", function (request, response){
+    const api_url = `http://www.boredapi.com/api/activity?type=${activity}`;
+    const fetch_response = await fetch(api_url);
+    const jsonObj = await fetch_response.json();
+    response.json(jsonObj);
+})
+app.listen(port, function (){
+    console.log(`App listening on http://localhost:${port}`);
+});
